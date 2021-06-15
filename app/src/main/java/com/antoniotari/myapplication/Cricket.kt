1792044c7694ab2de1549ef97f5ca953bfe18db9
antoniotari <antoniotari11@gmail.com>
2021-06-15 16:04:45 -0400

    tab with background state

---
+++ app/src/main/java/com/antoniotari/myapplication/Cricket.kt
@@ -0,0 +1,18 @@
+package com.antoniotari.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.q11.R
+
+class Cricket : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cricket, container, false)
+    }
+}
